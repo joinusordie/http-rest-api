95f4cc84c842878d9bc211d457b92c93572dbc0b
joinusordie <vengrisha2015@gmail.com>
2023-03-08 18:47:07 +0300

    Middleware components

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -4,5 +4,6 @@ import "github.com/joinusordie/http-restapi/internal/app/model"
 
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 } 
